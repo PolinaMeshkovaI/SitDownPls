0e1bdecdee2d8a77052411d6f972dab6ab7aafa0
Polina Meshkova <polina_i@bk.ru>
2023-04-06 19:48:32 +0700

    выполнен footer и устранены небольшие ошибки в БЭМ

--- src/js/components/show-more.js
+++ src/js/components/show-more.js
@@ -1,11 +1,11 @@
 export const showMore = () => {
   const showMore = document.querySelector('.high-rating__btn');
-  const productsLength = document.querySelectorAll('.card--rating').length;
+  const productsLength = document.querySelectorAll('.card__item--rating').length;
   let items = 8;
 
   showMore.addEventListener('click', () => {
     items += 4;
-    const array = Array.from(document.querySelector('.high-rating__wrapper').children);
+    const array = Array.from(document.querySelector('.card--rating').children);
     const visItems = array.slice(0, items);
 
     visItems.forEach(el => el.classList.add('is-visible'));
